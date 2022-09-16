22d40a5c95d49044dcc7be19f1bcd074f993178b
JasonGodmere <jgodmere808@gmail.com>
2022-09-16 09:55:38 -1000

    Added instance deletion button for each row, implemented delete opteration on database. works

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ import { db } from './db/index.js'
 app.use(express.static('public'))
 
 app.get('/instances', (req, res, next) => {
-	db.query('SELECT * FROM instances', (err, result) => {
+	let query = 'SELECT * FROM instances';
+	db.query(query, (err, result) => {
 		if (err) {
 			return next(err)
 		}
@@ -23,6 +24,19 @@ app.post('/instances', (req, res, next) => {
 	db.query(query, (err, result) => {
 		if (err) {
 			return next(err)
+		}
+		res.send(result);
+	})
+})
+
+app.delete('/instances/:id', (req, res, next) => {
+	let query = {
+		text: 'DELETE FROM instances WHERE id = $1',
+		values: [req.params.id]
+	};
+	db.query(query, (err, result) => {
+		if (err) {
+			return next(err);
 		}
 		res.send(result);
 	})
--- public/index.js
+++ public/index.js
@@ -1,10 +1,33 @@
 
 var server = "http://localhost:8888";
+var tableData = [];
 
-function updateTable(data) {	
+function createIndexId(id, index) {
+	return id + '-' + index;
+}
+function extractIndexId(id) {
+	return id.split('-')[-1];
+}
+
+function delBtnClick(id) {
+	let index = extractIndexId(id);
+	let instance_id = data[i][columns[0]];
+	console.log(instance_id);
+	console.log(i);
+	$.ajax({
+		url: server+'/instances/'+instance_id,
+		type: 'DELETE',
+		success: function(result) {
+			alert('Successfully deleted ' + instance_id);
+		}
+	});
+}
+
+function updateTable(data) {
+	tableData = data;
 	let table = document.getElementById('instance-table');
 	let rowClass = 'instance-table-item';
-	let columns = ['id', 'created', 'status']; // has to match sql column names
+	let columns = ['id', 'created', 'status', 'actions']; // has to match sql column names
 	
 	const rows = document.querySelectorAll('.'+rowClass);
 	rows.forEach(row => {
@@ -17,7 +40,32 @@ function updateTable(data) {
 
 		for (let j = 0; j < columns.length; j++) {
 			let td = document.createElement('td');
-			td.innerHTML = data[i][columns[j]];
+			switch (columns[j]) {
+			case 'actions':
+				let delBtn = document.createElement('button');
+				delBtn.innerHTML = 'DELETE';
+				delBtn.id = 'instance-delBtn-'+i;
+				delBtn.addEventListener('click', function(event) {
+					let id = event.target.id;
+					let index = extractIndexId(id);
+					let instance_id = data[i][columns[0]];
+					console.log(instance_id);
+					console.log(i);
+					$.ajax({
+						url: server+'/instances/'+instance_id,
+						type: 'DELETE',
+						success: function(result) {
+							document.getElementById('refresh').click();
+							alert('Successfully deleted ' + instance_id);
+						}
+					});
+				});
+				td.appendChild(delBtn);
+				break;
+			default:
+				td.innerHTML = data[i][columns[j]].slice(-12);
+				break;
+			}
 			tr.appendChild(td);
 		}
 		table.appendChild(tr);
@@ -37,6 +85,20 @@ $('#refresh').click(function() {
 		updateTable(data);
 		console.log(data);
 		console.log(status);
+	});
+});
+
+$('.instance-delBtn').on('click', function(event) {
+	let i = extractIndexId($(this).attr('id'));
+	let instance_id = data[i][columns[0]];
+	console.log(instance_id);
+	console.log(i);
+	$.ajax({
+		url: server+'/instances/'+instance_id,
+		type: 'DELETE',
+		success: function(result) {
+			alert('Successfully deleted ' + instance_id);
+		}
 	});
 });
 });
